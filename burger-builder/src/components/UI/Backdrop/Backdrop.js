3355f1165a94950b22bc6ba5e31044c116abb5a5
Greg Totsline <jtotsline@gmail.com>
2020-04-18 19:51:44 -0400

    docs

--- burger-builder/src/components/UI/Backdrop/Backdrop.js
+++ burger-builder/src/components/UI/Backdrop/Backdrop.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import classes from './Backdrop.css'
 
+/**
+ * The backdrop is a shaded non-clickable background use to indicate modality (e.g. dialog is up)
+ * @param {*} props 
+ */
 const backdrop = (props) => (
     props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
 );
