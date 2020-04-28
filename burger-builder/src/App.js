d12607c0c78ff66a9f2da8a8842df4adccb6b9d6
Greg Totsline <jtotsline@gmail.com>
2020-04-28 09:39:18 -0400

    Inital setup for routing

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom'
 import Layout from './containers/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
@@ -9,6 +10,7 @@ import Checkout from './containers/Checkout/Checkout'
  *   cd burger-builder
  *   npm run eject   // ejected so webpack.config.* can be changed to support scoped CCS classes
  * sudo npm install axios --save  // add support for rest calls from react using axios
+ * sudo npm install --save react-router react-router-dom // add support for path routing
  */
 class App extends Component {
 
@@ -24,12 +26,13 @@ class App extends Component {
   }
   render() {
     return (
-      
-      <Layout>
-       {this.state.show ? <BurgerBuilder /> : null}
-       <Checkout />
-      </Layout>
-    
+      // Setup BrowserRouter here so all descedant components can use routing 
+      <BrowserRouter>
+        <Layout>
+          {this.state.show ? <BurgerBuilder /> : null}
+          <Checkout />
+        </Layout>
+      </BrowserRouter>
     );
   }
 }
