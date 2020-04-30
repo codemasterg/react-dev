e961137cb27ac6cebe111cb1f0b3485624a4c72f
Greg Totsline <jtotsline@gmail.com>
2020-04-30 15:30:03 -0400

    Implemented generic input field config and 2-way binding.

---
+++ burger-builder/src/components/UI/Input/Input.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import classes from './Input.css'
+
+const input = (props) => {
+
+    let inputElement = null;
+
+    switch (props.elementType) {
+        case ('input'):
+            inputElement = <input 
+                className={classes.InputElement} 
+                {...props.elementConfig}
+                onChange={props.changed}
+                value={props.value}/>  // use spread to include type specifc props
+            break;
+        case('textarea'):
+            inputElement = <textarea 
+            className={classes.InputElement} 
+            {...props.elementConfig} 
+            onChange={props.changed}
+            value={props.value} />
+            break;
+        case ('select'):
+            inputElement = (<select
+                className={classes.InputElement}
+                onChange={props.changed}
+                value={props.value} >
+                    {props.elementConfig.options.map(option => (
+                        <option value={option.value} key={option.value} >
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
+        default:
+            break;
+    }
+
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+}
+
+
+export default input;
--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -1,23 +1,43 @@
-import React, {Component, Fragment} from 'react'
+import React, { Component, Fragment } from 'react'
 import axios from '../../../axios-orders'
 
-import Button from  '../../../components/UI/Button/Button'
+import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
 import Spinner from '../../../components/UI/Spinner/Spinner'
 import withErrorHandler from '../../../hoc/WithErrorHandler/withErrorHandler'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
 
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            zipCode: '',
+        orderForm: {
+            name: this.setupInput("text", "Your Name"),
+            street: this.setupInput("text", "Your Street"),
+            town: this.setupInput("text", "Your Town"),
+            zipCode: this.setupInput("text", "Your Zip"),
+            email: this.setupInput("email", "Your email@abc123.com"),
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'},
+                    ]
+                },
+                value: '',
+            },
         },
         loading: false,
     }
     render() {
+        const formElements = [];
+        for(let key in this.state.orderForm) {
+            formElements.push({
+                id: key,
+                config: this.state.orderForm[key],
+            });
+        }
+
         return (
             <Fragment>
                 {this.state.loading ? <Spinner /> :
@@ -25,10 +45,15 @@ class ContactData extends Component {
                     <div className={classes.ContactData}>
                         <h4>Enter your contact data</h4>
                         <form>
-                            <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-                            <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-                            <input className={classes.Input} type="text" name="street" placeholder="Your street" />
-                            <input className={classes.Input} type="text" name="zipCode" placeholder="Your zip" />
+                            {formElements.map(input => {
+                                return(
+                                    <Input elementType={input.config.elementType} 
+                                        elementConfig={input.config.elementConfig} 
+                                        value={input.config.value}
+                                        changed={(event) => this.inputChangedHandler(event, input.id)}
+                                        key={input.id} />
+                                );
+                            })}
                             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                         </form>
                     </div>
@@ -45,35 +70,55 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.totalPrice, // normally set on server!
-            customer: {
-                name: 'Greg',
-                address: {
-                    street: '100 Main St.',
-                    town: 'My Town',
-                    zipCode: 12345,
-                },
-                email: 'test@test.com',
-                deliveryMethod: 'mega-delivery'        
-            },
+        }
 
-            }
-        
         // base url set in instance that was imported
-        this.setState({loading: true}); // for busy wheel
+        this.setState({ loading: true }); // for busy wheel
         axios.post('/orders.json', order)
             .then(response => {
-                this.setState({loading: false});
+                this.setState({ loading: false });
                 console.log(response);
                 this.props.history.push('/');  // redirect back to main page
             })
             .catch(error => {
-                this.setState({loading: false});
+                this.setState({ loading: false });
                 console.log(error);
             })
-            .finally( () => {
+            .finally(() => {
                 // place holder.  cannot call setState here because sucess case does a
                 // redirect (history.push) which unmounts this component.
             })
+    }
+ 
+    // common input config creater
+    setupInput(inputType, placeholderText) {
+        return ({
+            elementType: 'input',
+            elementConfig: {
+                type: inputType,
+                placeholder: placeholderText,
+            },
+            value: '',
+        })
+    }
+
+    /**
+     * Perform 2-way binding of input field being changed (i.e. updated field value that
+     * is displayed with the user's input)
+     * @param {} event 
+     * @param {*} inputId 
+     */
+    inputChangedHandler(event, inputId) {
+        // console.log(event.target.value);
+
+        // Since spread operator does a shallow copy, do a 2nd spread to get element value copy
+        const updatedOrderForm = {...this.state.orderForm };
+        const updatedFormElement = {...updatedOrderForm[inputId] };
+
+        updatedFormElement.value = event.target.value;  // copy user input char(s)
+        updatedOrderForm[inputId] = updatedFormElement;  // The order form copy now has the updated input value
+        this.setState({orderForm: updatedOrderForm});   // note that the whole form is being updated
+
 
     }
 }
